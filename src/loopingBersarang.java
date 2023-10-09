ed1a840b41f2788f1f0596598f2758e594ac8152
KizaruZero <ardyapusaka@gmail.com>
2023-10-09 22:39:03 +0700

    ardya

---
+++ src/loopingBersarang.java
@@ -0,0 +1,65 @@
+public class loopingBersarang {
+    public static void main(String[] args) {
+        System.out.println("Menggambar Menggunakan Java");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        System.out.println("\n");
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
+                if (i == j) {
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
+        System.out.println("\n");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print("* ");
+                if ((i + j) == 4) {
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
+        System.out.print("\n");
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print("* ");
+                if (i == j) {
+                    break;
+                } else if ((i + j) == 8) {
+                    break;
+                }
+            }
+            System.out.print("\n");
+        }
+        System.out.println("TUGAS TEST OTAK BIAR GA GOBLOK");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i > j) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.print("\n");
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i + j <= 3) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.print("\n");
+        }
+    }
+}
